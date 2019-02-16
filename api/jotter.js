845e410145851b3e03725a30240e5e9b2138e3b9
Babatunde Mattieu <bbmattieu9@gmail.com>
2019-02-16 18:35:10 +0100

    Set Up Eslint Airbnb style guide Babel and created a route to test the app

---
+++ api/jotter.js
@@ -0,0 +1,11 @@
+/**
+  create ESLINT-----done
+  Create babel
+  create route file
+  create controller
+  create models.
+  do this tonait.
+  tomorrow
+  create dummy pages
+  good luck tizi!
+  */
